89abe77d4d23e31255599827422aaf4999936d1f
gdm <1875525594@qq.com>
2025-03-11 23:15:20 +0800

    网关设计

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# -GameGatewayServer
-12123
+# 游戏服务器框架 gameServer
+    JDK17
+    apache-maven-3.9.5
+    脚本工具:protoc-28.2-win64  https://github.com/protocolbuffers/protobuf/releases
+
+## 启动方式 
+    先在protobufFilem目录下执行脚本toJava.bat,然后对所有模块maven进行clean，install成功即可
+
+# 模块：
+# [common](common)
+    摘要 公共模块，底层核心模块，以及工具类的开发，开发待续.....
+## 注解 
+    @ToMethod:作用于类上，表明这个类为与客户端交互的类
+    
+    @ToServer:
+              1，作用于方法上，表明这个方法为与客户端响应的方法；
+              2，注解参数：协议唯一id
+              3，注意方法参数格式(ChannelHandlerContext,proto生成类,userId),第一个参数必须为ChannelHandlerContext,第二参数必须为proto生成类,第三参数为long类型userId
+              4，SendMsg.java 消息发送根据
+
+# [demo](demo)
+    为了演示增加的模块，里面有演示的示例，后续可删除，也可做业务模块
+
+# [entrance](entrance)
+    主启动类，程序入口,开发待续.....
+
+# [fight](fight)
+    战斗服，战斗逻辑，开发待续.....
+
+# [gateway](gateway)
+    网关服，开发待续....
+
+# [protobufFile](protobufFile)
+    存放protobuf文件，diy.proto里面有脚本toJava.bat用作生成java类
+
+# 目前本地测试可以轻松应付 1000线程， 2毫秒连接响应， 2毫秒回响应
+![img.png](img.png)
+![img_1.png](img_1.png)
---
+++ demo/src/main/java/com/slg/module/handle/Test.java
@@ -0,0 +1,84 @@
+package com.slg.module.handle;
+
+import com.slg.module.annotation.ToMethod;
+import com.slg.module.annotation.ToServer;
+import com.slg.module.interfaceT.monitor1.EventPublisher;
+import com.slg.module.message.MSG;
+import com.slg.module.message.SendMsg;
+import io.netty.channel.ChannelHandlerContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+@ToServer
+public class Test {
+    private static final Logger logger = LogManager.getLogger(Test.class);
+    private int sum = 0;
+    private SendMsg sendMsg;
+    @Autowired
+    EventPublisher publisher;
+    public Test(SendMsg sendMsg) {
+        this.sendMsg = sendMsg;
+    }
+
+    @ToMethod(value = 1)
+    public void diy(ChannelHandlerContext ctx, MSG.LoginRequest request,long userId) throws IOException {
+        sum++;
+        logger.debug("debug request:{}",request);
+        logger.info("info request:{}",request);
+        logger.warn("warn request:{}",request);
+        logger.error("error request:{}",request);
+        MSG.LoginResponse.Builder builder = MSG.LoginResponse.newBuilder();
+        byte[] byteArray = MSG.LoginResponse.newBuilder()
+                .setAaa(1111111111)
+                .setBbb(2132123132)
+                .buildPartial()
+                .toByteArray();
+
+        MSG.LoginResponse.Builder LoginResponseBuilder = MSG.LoginResponse.newBuilder()
+                .setAaa(999999999)
+                .setBbb(777777777);
+        sendMsg.send(ctx, LoginResponseBuilder);
+
+        sendMsg.send(ctx, byteArray);
+//        publisher.publishCustomEvent(LoginResponseBuilder);
+        if (sum > 9003) {
+            System.out.println("服务器收到数据sum" + sum);
+        }
+        if (sum == 70000) {
+            System.out.println("服务器收到数据sum" + sum);
+        }
+        if (sum == 80000) {
+            System.out.println("服务器收到数据sum" + sum);
+        }
+        if (sum == 85000) {
+            System.out.println("服务器收到数据sum" + sum);
+        }
+        if (sum == 90000) {
+            System.out.println("服务器收到数据sum" + sum);
+        }
+        if (sum >= 99900) {
+            System.out.println("服务器收到数据sum" + sum);
+        }
+    }
+
+
+    @ToMethod(value = 2)
+    public void ffff(ChannelHandlerContext ctx, MSG.FriendRequest request,long userId) throws IOException {
+        byte[] byteArray = MSG.LoginResponse.newBuilder()
+                .setAaa(1111111111)
+                .setBbb(2132123132)
+                .buildPartial()
+                .toByteArray();
+
+        MSG.LoginResponse.Builder LoginResponseBuilder = MSG.LoginResponse.newBuilder()
+                .setAaa(999999999)
+                .setBbb(777777777);
+        sendMsg.send(ctx, LoginResponseBuilder);
+        sendMsg.send(ctx, byteArray);
+    }
+
+}
